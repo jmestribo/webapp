14dc58db80c686be3312683949deded1ca9fcdeb
jmestribo <69851912+jmestribo@users.noreply.github.com>
2021-03-15 14:21:53 +0800

    adding body tag in index file

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 Sample
+<html>
+    <head>
+        <title>Webapp</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <h1>Sample</h1>
+    </body>
+</html>
